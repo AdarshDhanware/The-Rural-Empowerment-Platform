2997189454de2c6e9abafedcb423900c46ce48e6
Adarsh Dhanware <adarshdhanware@gmail.com>
2024-12-17 10:46:58 +0530

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000", // Backend URL
+        target: "https://the-rural-empowerment-platform-1.onrender.com", // Backend URL
         changeOrigin: true,
         secure: false,
       },
